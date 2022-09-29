1f9e96b258ee4ca1e4968a7f6f23dc90a5758ad6
BrainyQ <34138607+QABASAMER@users.noreply.github.com>
2022-09-29 13:35:11 +0300

    Update main.js

--- libs/main.js
+++ libs/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/assets/thirdTarget.mind',
+      imageTargetSrc: '../assets/thirdTarget.mind',
       maxTrack: 6,
     });
     const {renderer, scene, camera} = mindarThree;
@@ -16,27 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
     //const raccoon = await loadGLTF('/assets/landmine.gltf');
      //raccoon.scene.scale.set(0.0055, 0.0055, 0.0055);
     //raccoon.scene.position.set(0, -0.4, 0);
-    const raccoon = await loadGLTF('/assets/rusted.gltf');
+    const raccoon = await loadGLTF('../assets/rusted.gltf');
     raccoon.scene.scale.set(0.004, 0.004, 0.004);
     raccoon.scene.position.set(0, -0.4, 0);
 
-    const bear = await loadGLTF('/assets/Grenade.gltf');
+    const bear = await loadGLTF('../assets/Grenade.gltf');
     bear.scene.scale.set(0.1, 0.1, 0.1);
     bear.scene.position.set(0, -0.4, 0);
 
-    const object3 = await loadGLTF('/assets/landmine-uv.gltf');
+    const object3 = await loadGLTF('../assets/landmine-uv.gltf');
     object3.scene.scale.set(0.4, 0.4, 0.4);
     object3.scene.position.set(0, -0.4, 0);
 
-    const object4 = await loadGLTF('/assets/landminev2.gltf');
+    const object4 = await loadGLTF('../assets/landminev2.gltf');
     raccoon.scene.scale.set(0.004, 0.004, 0.004);
     object4.scene.position.set(0, -0.4, 0);
 
-    const object5 = await loadGLTF('/assets/mortar.gltf');
+    const object5 = await loadGLTF('../assets/mortar.gltf');
     raccoon.scene.scale.set(0.004, 0.004, 0.004);
     object5.scene.position.set(0, -0.4, 0);
 
-    const object6 = await loadGLTF('/assets/rusted.gltf');
+    const object6 = await loadGLTF('../assets/rusted.gltf');
     raccoon.scene.scale.set(0.0015, 0.0015, 0.0015);
     object6.scene.position.set(0, -0.4, 0);
 
